affa5c9d0cfff5b779cf6570cfedd6d1f838f879
Karl-Erik Kald <karl80038@gmail.com>
2020-09-09 10:52:54 +0300

    Added the ability where a randomly selected superhero fightsa randomly generated supervillain

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const superheroes = require('superheroes');
 const supervillains = require('supervillains');
+
 console.log(superheroes.all);
 console.log(superheroes.random());
 
 supervillains.all.forEach(supervillain => {
     console.log('My super villain is', supervillain);
 });
+var randomHero = superheroes.random();
+var randomVillain = supervillains.random();
+console.log("Superhero", randomHero, "is having a fight with", randomVillain,"!");
 console.log('Total of villains:', supervillains.all.length);
+
